1a7b1dad996ed77aa0a41f763e4a3442f1142495
Will Justis <will@jawdev.com>
2014-10-18 23:45:16 +0000

    cleanup

--- kohana/application/classes/Controller/Welcome.php
+++ kohana/application/classes/Controller/Welcome.php
@@ -4,7 +4,7 @@ class Controller_Welcome extends Controller {
 
 	public function action_index()
 	{
-		$this->response->body('hello, world!');
+		$this->response->body('hello, world!<br /><br />Maybe you are looking for the <a href="'.URL::site('projects').'">projects</a> page?');
 	}
 
 } // End Welcome
